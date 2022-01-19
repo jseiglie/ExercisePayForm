8a56b5cd2834221996c1aece2baaf5df73c27252
jseiglie <javierseiglie@gmail.com>
2022-01-19 12:36:25 +0000

    Validation added

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const myAlert = document.querySelector('.myAlert');
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            myAlert.classList.replace('dnone', 'dshow');
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+
